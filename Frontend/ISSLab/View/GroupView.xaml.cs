392d4dca1feb8bcf97dc754638228ad2550a3a63
Lorintiu Daniel <lorintiu.daniel@yahoo.com>
2024-05-11 17:00:39 +0300

    views added

---
+++ Frontend/ISSLab/View/GroupView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ISSLab.Model;
+using ISSLab.ViewModel;
+
+namespace ISSLab.View
+{
+    /// <summary>
+    /// Interaction logic for GroupView.xaml
+    /// </summary>
+        public partial class GroupView : UserControl
+        {
+        public GroupView()
+        {
+            InitializeComponent();
+        }
+        private void GroupSettingsButton_Click(object sender, RoutedEventArgs e)
+            {
+                GroupSettings.Visibility = Visibility.Visible;
+                GroupFeed.Visibility = Visibility.Collapsed;
+            }
+
+            private void GroupPostsButton_Click(object sender, RoutedEventArgs e)
+            {
+                GroupFeed.Visibility = Visibility.Visible;
+                PollsListBox.Visibility = Visibility.Collapsed;
+                PostsListBox.Visibility = Visibility.Visible;
+                GroupSettings.Visibility = Visibility.Collapsed;
+            }
+
+            private void CreatePollButton_Click(object sender, RoutedEventArgs e)
+            {
+                // TODO: Implement this
+            }
+
+            private void GroupPollsButton_Click(object sender, RoutedEventArgs e)
+            {
+                GroupFeed.Visibility = Visibility.Visible;
+                PollsListBox.Visibility = Visibility.Visible;
+                PostsListBox.Visibility = Visibility.Collapsed;
+                GroupSettings.Visibility = Visibility.Collapsed;
+            }
+        }
+}
---
+++ Frontend/ISSLab/View/PollCreationView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ISSLab.Model;
+using ISSLab.ViewModel;
+
+namespace ISSLab.View
+{
+    /// <summary>
+    /// Interaction logic for GroupView.xaml
+    /// </summary>
+    public partial class PollCreationView : UserControl
+    {
+        public PollCreationView()
+        {
+            InitializeComponent();
+        }
+
+        private void HasEndDateCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            EndDatePicker.Visibility = Visibility.Visible;
+        }
+
+        private void HasEndDateCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            EndDatePicker.Visibility = Visibility.Collapsed;
+        }
+    }
+}
