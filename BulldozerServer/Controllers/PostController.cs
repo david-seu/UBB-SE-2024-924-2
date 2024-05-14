bc196d598ad0f19152addeaeafc5958739a3c915
ErikMaidik914 <erik.maidik13@gmail.com>
2024-05-14 16:57:35 +0300

    addPost implemented in post controller

--- BulldozerServer/Controllers/PostController.cs
+++ BulldozerServer/Controllers/PostController.cs
@@ -1,9 +1,37 @@
-﻿using System;
+﻿using BulldozerServer.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
-public class PostController
+namespace BulldozerServer.Controllers
 {
-	public PostController()
-	{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostService : Controller
+    {
+        private readonly IPostRepository postRepository;
 
-	}
+        public PostService(IPostRepository postRepository)
+        {
+            this.postRepository = postRepository;
+        }
+
+        [HttpPost]
+        public IActionResult AddPost(Post post)
+        {
+            if (post == null)
+            {
+                return BadRequest("Post is null");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            postRepository.AddPost(post);
+
+            //return CreatedAtAction(nameof(AddPost), new { id = post.Id }, post);  Location header that contains the URI of the newly created post
+            return Ok();
+        }
+    }
 }
