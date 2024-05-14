afe435ead72b34c4372c8dceaa7addbe387afca1
RLSMname <115074855+RLSMname@users.noreply.github.com>
2024-05-14 18:36:35 +0300

    added getposts form postservice

---
+++ BulldozerServer/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using BulldozerServer.Domain;
+
+namespace BulldozerServer.Controllers
+{
+    [ApiController] // /api/PostService
+    [Route("api/[controller]")]
+    public class PostService : Controller
+    {
+        private readonly IPostRepository postRepository;
+
+        public PostService(IPostRepository postRepository)
+        {
+            this.postRepository = postRepository;
+        }
+
+        [HttpGet("GetPosts")]
+        public IEnumerable<MarketplacePost> GetPosts()
+        {
+            return postRepository.GetAllPosts();
+        }
+    }
+
+}
