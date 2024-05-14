68277655d075786bfa7ae3713780d6c3e0f0432e
Suciu-Bogdan <115088448+suciubogdansb@users.noreply.github.com>
2024-05-14 20:20:23 +0300

    Created domain structure for marketplace posts (migration still required for table creation))

---
+++ BulldozerServer/Domain/MarketplacePosts/MarketplacePost.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulldozerServer.Domain.MarketplacePosts
+{
+    public class MarketplacePost
+    {
+        private Guid marketplacePostId;
+        private Guid authorId;
+        private Guid groupId;
+        private string title;
+        private string description;
+        private string mediaContent;
+        private string location;
+        private DateTime creationDate;
+        private DateTime? endDate;
+        private bool isPromoted;
+        private bool isActive;
+
+        [Key]
+        public Guid MarketplacePostId { get => marketplacePostId; }
+
+        public Guid AuthorId { get => authorId; set => authorId = value; }
+        public Guid GroupId { get => groupId; set => groupId = value; }
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
+        public string MediaContent { get => mediaContent; set => mediaContent = value; }
+        public string Location { get => location; set => location = value; }
+        public DateTime CreationDate { get => creationDate; set => creationDate = value; }
+        public DateTime? EndDate { get => endDate; set => endDate = value; }
+        public bool IsPromoted { get => isPromoted; set => isPromoted = value; }
+        public bool IsActive { get => isActive; set => isActive = value; }
+
+        public MarketplacePost(Guid marketplacePostId, Guid authorId, Guid groupId, string title, string description, string mediaContent,
+            string location, DateTime creationDate, DateTime? endDate, bool isPromoted, bool isActive)
+        {
+            this.marketplacePostId = marketplacePostId;
+            this.authorId = authorId;
+            this.groupId = groupId;
+            this.title = title;
+            this.description = description;
+            this.mediaContent = mediaContent;
+            this.location = location;
+            this.creationDate = creationDate;
+            this.endDate = endDate;
+            this.isPromoted = isPromoted;
+            this.isActive = isActive;
+        }
+
+        public MarketplacePost() {
+            this.marketplacePostId = Guid.NewGuid();
+            this.authorId = Guid.NewGuid();
+            this.groupId = Guid.NewGuid();
+            this.title = Constants.EMPTY_STRING;
+            this.description = Constants.EMPTY_STRING;
+            this.mediaContent = Constants.EMPTY_STRING;
+            this.location = Constants.EMPTY_STRING;
+            this.creationDate = DateTime.Now;
+            this.endDate = null;
+            this.isPromoted = false;
+            this.isActive = true;
+        }
+    }
+}
