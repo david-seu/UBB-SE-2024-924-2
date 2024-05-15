924bd479862adce1cc8da508b817019d6fc66fbd
RLSMname <115074855+RLSMname@users.noreply.github.com>
2024-05-15 11:20:48 +0300

    copy pasted services

--- BulldozerServer/Services/IPostService.cs
+++ BulldozerServer/Services/IPostService.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace BulldozerServer.Services
+{
+    public interface IPostService
+    {
+        void AddPost(MarketplacePost marketplacePost);
+        void ConfirmPost(Guid postID);
+        void FavoritePost(Guid postID, Guid userID);
+        MarketplacePost GetPostById(Guid id);
+        List<MarketplacePost> GetPosts();
+        void RemoveConfirmation(Guid postID);
+        void RemovePost(MarketplacePost marketplacePost);
+        void UnfavoritePost(Guid postID, Guid userID);
+    }
+}
