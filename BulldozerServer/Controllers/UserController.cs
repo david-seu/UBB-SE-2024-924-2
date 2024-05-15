b0b8ce19c58a6bf16e9778e273cd454629ab48cd
RLSMname <115074855+RLSMname@users.noreply.github.com>
2024-05-15 11:25:26 +0300

    added empty user controller

---
+++ BulldozerServer/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using BulldozerServer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulldozerServer.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+}
