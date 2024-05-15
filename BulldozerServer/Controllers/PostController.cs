58db3cde25d058554547877145d65cb68d2198b7
RLSMname <115074855+RLSMname@users.noreply.github.com>
2024-05-15 11:15:34 +0300

    empty postcontroller

---
+++ BulldozerServer/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulldozerServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostController : Controller
+    {
+        private readonly IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+    }
+}
