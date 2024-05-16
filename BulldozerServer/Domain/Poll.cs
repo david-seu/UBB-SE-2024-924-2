0e81cf0baf3b80c54576d6038ef71c9f1afba6e8
Suciu-Bogdan <115088448+suciubogdansb@users.noreply.github.com>
2024-05-16 22:10:44 +0300

    modified something

--- BulldozerServer/Domain/Poll.cs
+++ BulldozerServer/Domain/Poll.cs
@@ -13,13 +13,13 @@ namespace BulldozerServer.Domain
         private Guid groupId;
         private string description;
         private DateOnly creationDate;
-        private DateOnly endDate;
+        private DateOnly? endDate;
         private bool isPinned;
         private bool isVisible;
         private bool isMultipleChoice;
         private bool isAnonymous;
 
-        public Poll(Guid groupId, string description, DateOnly creationDate, DateOnly endDate, bool isPinned, bool isVisible,
+        public Poll(Guid groupId, string description, DateOnly creationDate, DateOnly? endDate, bool isPinned, bool isVisible,
             bool isMultipleChoice, bool isAnonymous)
         {
             this.pollId = Guid.NewGuid();
@@ -39,14 +39,14 @@ namespace BulldozerServer.Domain
             this.groupId = Guid.NewGuid();
             this.description = Constants.EMPTY_STRING;
             this.creationDate = DateOnly.FromDateTime(DateTime.Now);
-            this.endDate = DateOnly.FromDateTime(DateTime.Now);
+            this.endDate = null;
             this.isPinned = false;
             this.isVisible = false;
             this.isMultipleChoice = false;
             this.isAnonymous = false;
         }
 
-        public Poll(Guid pollId, Guid groupId, string description, DateOnly creationDate, DateOnly endDate, bool isPinned,
+        public Poll(Guid pollId, Guid groupId, string description, DateOnly creationDate, DateOnly? endDate, bool isPinned,
             bool isVisible,
             bool isMultipleChoice, bool isAnonymous)
         {
@@ -65,7 +65,7 @@ namespace BulldozerServer.Domain
         public Guid GroupId { get => groupId; }
         public string Description { get => description; set => description = value; }
         public DateOnly CreationDate { get => creationDate; }
-        public DateOnly EndDate { get => endDate; set => endDate = value; }
+        public DateOnly? EndDate { get => endDate; set => endDate = value; }
         public bool IsPinned { get => isPinned; set => isPinned = value; }
         public bool IsVisible { get => isVisible; set => isVisible = value; }
         public bool IsMultipleChoice { get => isMultipleChoice; set => isMultipleChoice = value; }
