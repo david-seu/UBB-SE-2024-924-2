ea4d24a4e75059514fcca8062039e322841171be
ErikMaidik914 <erik.maidik13@gmail.com>
2024-05-16 14:39:15 +0300

    domain backend entities

---
+++ BulldozerServer/Domain/PollAnswer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulldozerServer.Domain
+{
+    public class PollAnswer
+    {
+        private Guid pollAnswerId;
+        private Guid userId;
+
+        public PollAnswer(Guid userId)
+        {
+            this.pollAnswerId = Guid.NewGuid();
+            this.userId = userId;
+        }
+
+        public PollAnswer()
+        {
+            this.pollAnswerId = Guid.NewGuid();
+            this.userId = Guid.NewGuid();
+        }
+
+        public PollAnswer(Guid pollAnswerId, Guid userId)
+        {
+            this.pollAnswerId = pollAnswerId;
+            this.userId = userId;
+        }
+
+    }
+}
---
+++ BulldozerServer/Domain/PollOption.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulldozerServer.Domain
+{
+    public class PollOption
+    {
+        private Guid pollOptionId;
+        private Guid pollId;
+        private string option;
+
+        public PollOption(Guid pollId, string option)
+        {
+            this.pollOptionId = Guid.NewGuid();
+            this.pollId = pollId;
+            this.option = option;
+        }
+
+        public PollOption()
+        {
+            this.pollOptionId = Guid.NewGuid();
+            this.pollId = Guid.NewGuid();
+            this.option = Constants.EMPTY_STRING;
+        }
+
+        public PollOption(Guid pollOptionId, Guid pollId, string option)
+        {
+            this.pollOptionId = pollOptionId;
+            this.pollId = pollId;
+            this.option = option;
+        }
+
+        public Guid PollOptionId { get => pollOptionId; }
+        public Guid PollId { get => pollId; }
+        public string Option { get => option; set => option = value; }
+    }
+}
