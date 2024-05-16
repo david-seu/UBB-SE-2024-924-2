0e8b38c3531e1cc90f9607286d613e8073aa9111
David Seu 916 <david.seu@stud.ubbcluj.ro>
2024-05-16 21:57:14 +0300

    asdfa

--- BulldozerServer/Mapper/GroupMapper.cs
+++ BulldozerServer/Mapper/GroupMapper.cs
@@ -9,7 +9,7 @@ namespace BulldozerServer.Mapper
         {
             GroupDTO groupDTO = new GroupDTO();
             groupDTO.GroupId = group.GroupId;
-            groupDTO.UserId = group.UserId;
+            groupDTO.UserId = group.OwnerId;
             groupDTO.CreatedDate = group.CreatedDate;
             groupDTO.Description = group.Description;
             groupDTO.GroupName = group.GroupName;
@@ -18,7 +18,7 @@ namespace BulldozerServer.Mapper
             return groupDTO;
         }
 
-        public static Group GroupDTOToJoinRequest(GroupDTO groupDTO)
+        public static Group GroupDTOToGroup(GroupDTO groupDTO)
         {
             return new Group(groupDTO.GroupId, groupDTO.UserId, groupDTO.GroupName, groupDTO.Description, groupDTO.CreatedDate, groupDTO.IsPublic, groupDTO.AllowanceOfPostage);
         }
