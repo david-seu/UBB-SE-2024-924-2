69f3bb13c7ea30570ea61ca17363a94994f47525
Linzing Andrei <andrei.linzing@gmail.com>
2024-05-16 14:44:43 +0300

    changes

--- BulldozerServer/Payload/DTO/MarketplacePostDTO.cs
+++ BulldozerServer/Payload/DTO/MarketplacePostDTO.cs
@@ -1,4 +1,8 @@
-﻿namespace BulldozerServer.Payloads.DTO
+﻿using BulldozerServer.Domain;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BulldozerServer.Payloads.DTO
 {
     public class MarketplacePostDTO
     {
@@ -14,6 +18,30 @@
         private bool isPromoted;
         private bool isActive;
 
+        [Key]
+        public Guid MarketplacePostId { get => marketplacePostId; }
 
+        [AllowNull]
+        public Guid? AuthorId { get => authorId; set => authorId = value; }
+        public Guid GroupId { get => groupId; set => groupId = value; }
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
+        [AllowNull]
+        public string? MediaContent { get => mediaContent; set => mediaContent = value; }
+        [AllowNull]
+        public string? Location { get => location; set => location = value; }
+        public DateTime CreationDate { get => creationDate; set => creationDate = value; }
+        [AllowNull]
+        public DateTime? EndDate { get => endDate; set => endDate = value; }
+        public bool IsPromoted { get => isPromoted; set => isPromoted = value; }
+        public bool IsActive { get => isActive; set => isActive = value; }
+
+        public User? Author { get; set; }
+
+        public Group Group { get; set; }
+
+        public ICollection<User> PeopleThatFavored { get; } = new List<User>();
+
+        public ICollection<User> PeopleThatPlacedInCart { get; } = new List<User>();
     }
 }
